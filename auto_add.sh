e5e5873fe165b79de8dc214929db6775e335137a
rachpt <rachpt@126.com>
2018-05-19 15:45:52 +0800

    修复 bugs

--- auto_add.sh
+++ auto_add.sh
@@ -2,34 +2,37 @@
 # FileName: auto_add.sh
 #
 # Author: rachpt@126.com
-# Version: 1.0v
-# Date: 2018-05-17
-
+# Version: 1.2v
+# Date: 2018-05-19
+#
 #-------------settings---------------#
-#---transmissionreemote---#
-trans_remote='transmission-remote'
+# Do not move it!
+torrent2add="${download_url}&passkey=${passkey}"
 
-ratio='10'
-
-HOST='127.0.0.1'
-PORT='9090'
-USER='admin'
-PASSWORD='password'
-trorrent2add="$1"
+#-------------functions---------------#
+function set_ratio()
+{
+    for eachTorrentID in `$trans_remote ${HOST}:${PORT} --auth ${USER}:${PASSWORD} -l|grep %| awk '{print $1}'`
+    do
+	    eachTorrent=`$trans_remote ${HOST}:${PORT} --auth ${USER}:${PASSWORD} -t $eachTorrentID -i |grep Name|awk '{print $2}'`
+	    set_commit=`$trans_remote ${HOST}:${PORT} --auth ${USER}:${PASSWORD} -t $eachTorrentID -i |grep "hudbt.hust.edu.cn"`
+        if [ "$TR_TORRENT_NAME" = "$eachTorrent" ] && [ -n "$set_commit" ]; then
+		    $trans_remote ${HOST}:${PORT} -n ${USER}:${PASSWORD} -t $eachTorrentID -sr $ratio
+        fi
+    done
+}
 
 function add_torrent()
 {
-
-    $trans_remote ${HOST}:${PORT} -n ${USER}:${PASSWORD} --add "$torrent2add" -w "$TR_TORRENT_DIR" -sr "$ratio" -s --trash-torrent
-
-
+    $trans_remote ${HOST}:${PORT} -n ${USER}:${PASSWORD} --add "$torrent2add" -w "$TR_TORRENT_DIR"
+    sleep 10
+    set_ratio
 }
-
 
 #-------------call function---------------#
 
-if [ "$1" ]; then
+if [ "$torrent2add" ]; then
+	echo "++++++++++++++[add]++++++++++++++" >> $logoPath
     add_torrent
-
     . ./auto_clean.sh
 fi
