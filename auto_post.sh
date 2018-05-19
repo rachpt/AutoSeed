5e3a1c792041b55fda83439d0eee67c0f8f1b1d3
rachpt <rachpt@126.com>
2018-05-19 15:46:53 +0800

    修复多个错误

--- auto_post.sh
+++ auto_post.sh
@@ -2,96 +2,92 @@
 # FileName: auto_post.sh
 #
 # Author: rachpt@126.com
-# Version: 1.0v
-# Date: 2018-05-17
-
+# Version: 1.2v
+# Date: 2018-05-19
+#
 #-------------settings---------------#
-h2b='html2bbcode'
 
-torrentPath="${flexget_path}$1"
-
-#---watch dir(full, like:/home/down/)---#
-watch_dir='/home/rc/Downloads/hudbt/'
-
-name="Default Title"
-
-smallDescr="Default subtitle"
-
-postUrl='https://hudbt.hust.edu.cn/takeupload.php'
-
-descrCom="[quote]
-[b]这是一个自动发布的种子[/b]
-[ul]
-[li]所有信息以所发种子信息(文件名)为准，所有标题、简介信息均仅供参考，若发现有误请以[举报]或[留言]的形式通知工作人员审查和编辑。
-[*]保种12-20天，断种恕不补种。
-[*] Shell 脚本实现，具体见：[url=https://github.com/rachpt/AutoSeed]GitHub AutoSeed[/url]
-[/li][/ul]
-[/quote]\n\n[hr /]"
-
-imdbUrl=''
-
-#---default type---#
-selectType=430
-
-standardSel=3
-
-anonymous='yes'
-
-#---cookie of forwarding site---#
-cookie='----string-----'
+torrentPath="${flexget_path}${new_torrent_name}.torrent"
 
 #-------------------------------------#
-
 #---paramter torrent's dot connected name---#
 function get_descr()
 {
     offset_A=3
     offset_B=7
     #---get torrent's name(without dot)---#
-    if [ "$1" ]; then
-        name=`sed 's/\./ /g' "${1%./}"`
+    if [ "$new_torrent_name" ]; then
+        name=`echo "$new_torrent_name"|sed 's/\./ /g'`
     else
-        name="Failed to get Title"
+        name="$default_name"
     fi
-
+    
+    #---get torrent's type---#
+    case "$name" in
+        *iPad*)
+            selectType='430' ;;
+        *ipad*)
+            selectType='430' ;;
+        *)
+            selectType="$default_select_type"  ;;
+    esac
+    
+    #---get torrent's standard---#
+    case "$name" in
+        *720p*)
+            standardSel='3' ;;
+        *1080p*)
+            standardSel='1' ;;
+        *)
+            standardSel="$default_standard"  ;;
+    esac 
+    
     #---get item arrar---#
-    html_page=`mktemp /tmp/rssTempPage.XXXXXXXXXX`
+    html_page=`mktemp /tmp/RssTempPage.XXXXXXXXXX`
+    curl -s --connect-timeout 100 -m 300 "https://hdsky.me/torrentrss.php?rows=50" > $html_page
     j=0
-    for i in `grep -n \<item\> $html_page|cut -d: -f1`
+    for i in `grep -n '<item>' $html_page|cut -d: -f1`
     do
-        all_item_lists_A[j]=$i
+        all_item_lists_A[$j]=$i
         j=`expr $j + 1`
     done
     j=0
-    for i in `grep -n \</item\> $html_page|cut -d: -f1`
+    for i in `grep -n '</item>' $html_page|cut -d: -f1`
     do
-        all_item_lists_B[j]=$i
+        all_item_lists_B[$j]=$i
         j=`expr $j + 1`
     done
-
+    #echo step 3 "${all_item_lists_A[@]}">> $logoPath
     #---get current item---#
     
-    torrent_location_line=`grep -n "$name" $html_page|cut -d: -f1`
+    torrent_location_line=`grep -n "$name" $html_page|cut -d: -f1|head -n 1`
     j=0
-    while [ $j lt 50 ]
+    while [ $j -lt 50 ]
     do
-        if [ $torrent_location_line -ge $all_item_lists_A[$j] ] && [ $torrent_location_line -le $all_item_lists_B[$j] ]; then
-            min_item_line=$all_item_lists_A[$j]
-            max_item_line=$all_item_lists_B[$j]
+        if [ $torrent_location_line -ge "${all_item_lists_A[$j]}" ] && [ $torrent_location_line -le "${all_item_lists_B[$j]}" ]; then
+            min_item_line=${all_item_lists_A[$j]}
+            max_item_line=${all_item_lists_B[$j]}
             break
         fi
+        #echo in it $j >> $logoPath
+        j=`expr $j + 1`
     done
-
+    echo $torrent_location_line == $j step 4 $min_item_line $max_item_line >> $logoPath
     #---extral item's descr---#
 
-    descr_page=`mktemp /tmp/torrentDescr.XXXXXXXXXX`
-    descr_bbcode=`mktemp /tmp/BBCode.XXXXXXXXXX``
-    sed -n "$(expr $min_item_line + $offset_A),$(expr $max_item_line - $offset_B)p" $html_page > $descr_page
+    descr_page=`mktemp /tmp/TorrentDescr.XXXXXXXXXX`
+    descr_bbcode=`mktemp /tmp/BBCode.XXXXXXXXXX`
+    sed -n "`expr $min_item_line + $offset_A`, `expr $max_item_line - $offset_B` p" $html_page > $descr_page
+    sleep 1
+
     sed -i "s/<description><\!\[CDATA\[//g; s/\]\]><\/description>//g" $descr_page
     "$h2b" $descr_page $descr_bbcode
-
+    sleep 1
 
     imdbUrl=`grep tt[0-9][0-9][0-9][0-9][0-9][0-9][0-9] $descr_page |awk 'BEGIN{FS="//"} {print $2}'|head -n 1|awk 'BEGIN{FS="/"}{print $3}'`
+    if [ -z "$imdbUrl" ]; then
+	    imdbUrl="$default_imdb_url"
+    fi
     
     #---filter bbdcode---#
 
@@ -99,7 +95,12 @@ function get_descr()
     sed -i 'N;s/^?\n?/\n/g' $descr_bbcode 
     sed -i 'N;s/?\n?$//g' $descr_bbcode
     sed -i 's/?$//' $descr_bbcode
-    sed -i 's#\[font=.*\]#\n[quote]#g;s#\[/font\]#[/quote]\n\n#g' $descr_bbcode
+    sleep 1
+    if [ -n "$(grep '引用' $descr_bbcode)" ]; then
+        sed -i 's#\引用#[quote]#g;s#\[/font\]#[/font][/quote]\n#g' $descr_bbcode
+    else
+        sed -i 's#\[font=.*\]#[quote][font=monospace]#g;s#\[/font\]#[/font][/quote]\n#g' $descr_bbcode
+    fi
     sed -i 's/^[\t ]*\[img/\[img/' $descr_bbcode
 
     #---get subtitle---#
@@ -109,43 +110,61 @@ function get_descr()
         smallDescr="$name_1"
     elif [ "$name_2" ]; then
         smallDescr="$name_2"
+    else
+        smallDescr="$default_subname"
     fi
+    
+    #---join descr---#
+    des="${descrCom}
+    `cat $descr_bbcode`"
+    
+    #---log---#
+    echo "+++++++++++[post data]+++++++++++" >> $logoPath
+    echo -e name="$name" "\n" small_descr="$smallDescr" "\n" url="$imdbUrl" "\n" type=$selectType "\n" standard_sel=$standardSel "\n" uplver="$anonymous"  >> $logoPath
 
 }
-
 
 #-------------------------------------#
 #---paramter up_status code---#
 function upload_torrent()
 {
-    if [ "$1" = "1" ]; then
-        t_id=`http -fh POST $postUrl name="$name" small_descr="$smallDescr" url="$imdbUrl" descr="${descrCom}$(cat $descr_bbcode)" type=$selectType standard_sel=$standardSel uplver="$anonymous" file@$torrentPath "$cookie" | grep id= |cut -d = -f 2|cut -d & -f 1` 
-    
+    if [ "$up_status" = "1" ]; then
+        t_id=`http -f -h POST $postUrl 'name'="$name" 'small_descr'="$smallDescr" 'url'="$imdbUrl" 'descr'="$des" 'type'="$selectType" 'standard_sel'="$standardSel" 'uplver'="$anonymous" file@"${torrentPath}" "$cookie" | grep "id=" |head -n 1|cut -d '=' -f 2|cut -d '&' -f 1` 
+
+        echo t_id: [$t_id] >> $logoPath
     
         if [ -z "$t_id" ]; then
-    	    t_id=`http -f POST $postUrl name="test" small_descr="something" descr="1111111" type="430" uplver="$anonymous" file@"$torrentPath" "$cookie"|grep hit=1|cut -d = -f 5|cut -d '&' -f 1`
+    	    t_id=`http -f POST $postUrl name="$name" small_descr="$smallDescr" url="$imdbUrl" descr="$des" type="430" uplver="$anonymous" file@"$torrentPath" "$cookie"|grep hit=1|head -n 1|cut -d = -f 5|cut -d '&' -f 1`
+    	    echo reget t_id:  [$t_id] >> $logoPath
         fi
+        
+        if [ -z "$t_id" ]; then
+    	    t_id=`http -f POST $postUrl name="$name" small_descr="$smallDescr" url="$imdbUrl" descr="$des" type="430" uplver="$anonymous" file@"$torrentPath" "$cookie"|grep hit=1|head -n 1|cut -d = -f 5|cut -d '&' -f 1`
+    	    echo reget t_id:  [$t_id] >> $logoPath
+        fi
+    	
+    	download_url="https://hudbt.hust.edu.cn/download.php?id=$t_id"
     
-    	download_url='https://hudbt.hust.edu.cn/download.php?id='$t_id
-    
-    	http -d $download_url -o "${watch_dir}${t_id}.torrent" "$cookie" 
     fi
 }
 
 function finish()
 {
+	echo "+++++++++++[delet tmp]+++++++++++" >> $logoPath
     rm -rf $html_page $descr_page $descr_bbcode 
 }
 
-
 #----------call function-----------#
 
-get_descr "$1"
+get_descr
 
-upload_torrent "$2"
+upload_torrent
 
-if [ -f "${watch_dir}${t_id}.torrent" ]; then
-    . ./auto_add.sh "${watch_dir}${t_id}.torrent"
+if [ -n "${t_id}" ]; then
+    . ./auto_add.sh
+    rm -f $torrentPath # delete uploaded torrent
+else
+    echo "++++++++++[failed to get tID]++++++++++" >> $logoPath
 fi
 
 #--------------exit----------------#
