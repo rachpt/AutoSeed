3d99554b9c49736d7470da1f2db8bcf4762a03a5
rachpt <rachpt@126.com>
2018-05-19 15:48:14 +0800

    添加调试代码

---
+++ debug.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# FileName: debug.sh
+#
+# Author: rachpt@126.com
+# Version: 1.2v
+# Date: 2018-05-19
+#
+#------------------------------------#
+#---path---#
+TR_TORRENT_DIR='/srv/ftp/mp4'
+#---real name of torrent(inclode dot always)---#
+TR_TORRENT_NAME='Miracles.of.the.Namiya.General.Store.2017.Repack.BluRay.iPad.720p.AAC.x264-HDSPad'
+
+. ./auto_main.sh
