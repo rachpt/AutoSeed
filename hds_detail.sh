5d1e75c4f443a09d0bcd4d68c3e129f22d808e56
rachpt <rachpt@126.com>
2018-06-04 20:20:58 +0800

    修复错误

--- hds_detail.sh
+++ hds_detail.sh
@@ -2,11 +2,11 @@
 # FileName: hds_detail.sh
 #
 # Author: rachpt@126.com
-# Version: 1.5v
-# Date: 2018-05-23
+# Version: 1.6v
+# Date: 2018-06-04
 #
 #-------------------------------------#
-hds_html_page=`mktemp /tmp/HDS.detail.XXXXXXXXXX`
+hds_html_page=`mktemp /tmp/hds.detail.XXXXXXXXXX`
 
 hds_t_id=`http "https://hdsky.me/torrents.php?search=$new_torrent_name" "Cookie:$cookie_hds"|grep 'href="details.php?id='|head -n 1|awk -F 'id=' '{print $2}'|awk -F '&amp' '{print $1}'`
 
@@ -16,7 +16,7 @@ if [ -n "$hds_t_id" ]; then
     start_line_html=`grep -n '</tr></table></td></tr>' "$hds_html_page" |head -n 1|awk -F : '{print $1}'`
     end_line_html=`grep -n '</div></td></tr>$' "$hds_html_page" |head -n 1|awk -F : '{print $1}'`
     
-    sed -n "`expr ${start_line_html} + 1`,${end_line_html} p" $hds_html_page|sed 's#.*lt="ad" /></a></div>##' > $descr_page
+    sed -n "`expr ${start_line_html} + 1`,${end_line_html} p" $hds_html_page|sed 's#.*lt="ad" /></a></div>##; /<tr><td class="rowhead">豆瓣*/d; s/<\/div><\/td><\/tr>//' > $descr_page
 
 else
     echo failed to get hds t_id $hds_t_id >> $log_Path
