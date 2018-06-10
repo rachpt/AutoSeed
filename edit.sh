72fc05f960907c43223a03580fe4e83eb98cd494
rachpt <rachpt@126.com>
2018-06-11 00:10:25 +0800

    升级至v2.0

--- edit.sh
+++ edit.sh
@@ -2,54 +2,73 @@
 # FileName: edit.sh
 #
 # Author: rachpt@126.com
-# Version: 1.6v
-# Date: 2018-06-04
+# Version: 2.0v
+# Date: 2018-06-08
 #
 #------------------------------------#
-AUTO_ROOT_PATH='/home/rachpt/shell/auto'
-source /etc/profile
-source ~/.bashrc
-source ~/.profile
-
 if [ -z "$log_Path"]; then
+    AUTO_ROOT_PATH="$(dirname "$(readlink -f "$0")")"
     source "$AUTO_ROOT_PATH/settings.sh"
+    echo $AUTO_ROOT_PATH
 fi
 #----------------post----------------#
-function edit_post()
+function edit_post_normal()
 {
-    http --ignore-stdin -f POST $edit_postUrl 'id'="$t_id"  'name'="$name" 'small_descr'="$smallDescr" 'url'="$imdbUrl" 'nfoaction'='keep' 'descr'="$des" 'type'="$selectType" 'standard_sel'="$standardSel" 'visible'="1" "$cookie"
-
+    http --ignore-stdin -f POST "$edit_postUrl" 'id'="$t_id"  'name'="$no_dot_name" 'small_descr'="$smallDescr" 'url'="$imdbUrl" 'nfoaction'='keep' 'descr'="$complex_des" 'type'="$selectType" 'standard_sel'="$standardSel" 'visible'="1" "$cookie"
 }
+function edit_post_npupt()
+{
+    http --ignore-stdin -f POST "$edit_postUrl" 'id'="$t_id"  'name'="$dot_name" 'small_descr'="$smallDescr" 'nfoaction'='keep' 'descr'="$npupt_des" 'type'="$selectType" 'source_sel'="$npupt_select_source" 'visible'="1" "$cookie"
+}
+function edit_post_nanyangpt()
+{
+    http --ignore-stdin -f POST "$edit_postUrl" 'id'="$t_id"  'name'="$dot_name" 'small_descr'="$smallDescr" 'url'="$imdbUrl" 'nfoaction'='keep' 'descr'="$nanyangpt_des" 'type'="$selectType" 'visible'="1" "$cookie"
+}
+
 #------------------------------------#
-for edit_loop in `egrep -n "small_descr=$default_subname_whu|small_descr=$default_subname_hudbt" $log_Path |awk -F : '{print $1}'`
+for edit_loop in `egrep -n "small_descr=$default_subname" "$log_Path" |awk -F ':' '{print $1}'`
 do
-    name=`sed -n "$(expr $edit_loop - 1) p" $log_Path|awk -F '=' '{print $2}'`
-    new_torrent_name=`echo "$name"|sed 's/ /./g'`
-    check_site="`sed -n "$(expr $edit_loop + 5) p" $log_Path`"
-    t_id=`sed -n "$(expr $edit_loop + 6) p" $log_Path|awk -F '[' '{print $2}'|awk -F ']' '{print $1}'`
+    posted_name="`sed -n "$(expr $edit_loop - 1) p" "$log_Path"|awk -F '=' '{print $2}'`"
+    new_torrent_name="`echo "$posted_name"|sed 's/ /./g'`"
+    check_site="`sed -n "$(expr $edit_loop + 3) p" "$log_Path"`"        # post site
+    source_site_URL="`sed -n "$(expr $edit_loop + 4) p" "$log_Path"`"   # source site
+    t_id=`sed -n "$(expr $edit_loop + 5) p" "$log_Path"|awk -F '[' '{print $2}'|awk -F ']' '{print $1}'`
 
     if [ "$t_id" ]; then
-        source "$AUTO_ROOT_PATH/hds_desc.sh"
-        echo $imdbUrl 
+    	source "$AUTO_ROOT_PATH/get_desc/detail_page.sh"
+	    source "$AUTO_ROOT_PATH/post/param.sh"
+
+        echo $imdbUrl
         if [ "$check_site" = "https://hudbt.hust.edu.cn" ]; then
-            source "$AUTO_ROOT_PATH/hds2hudbt.sh"
-            edit_post
+            source "$AUTO_ROOT_PATH/post/hudbt.sh"
+            echo "$edit_postUrl" 'id'="$t_id"  'name'="$no_dot_name" 'small_descr'="$smallDescr" 'url'="$imdbUrl" 'nfoaction'='keep' 'descr'="$complex_des" 'type'="$selectType" 'standard_sel'="$standardSel" 'visible'="1" "$cookie"
+            edit_post_normal
             echo "$default_subname" "$smallDescr"
             sed -i "${edit_loop}s#$default_subname#$smallDescr#" "$log_Path"
         elif [ "$check_site" = "https://pt.whu.edu.cn" ]; then
-            source "$AUTO_ROOT_PATH/hds2whu.sh"
-            edit_post
+            source "$AUTO_ROOT_PATH/post/whu.sh"
+            edit_post_normal
+            echo "$default_subname" "$smallDescr"
+            sed -i "${edit_loop}s#$default_subname#$smallDescr#" "$log_Path"
+        elif [ "$check_site" = "https://npupt.com" ]; then
+            source "$AUTO_ROOT_PATH/post/npupt.sh"
+            edit_post_npupt
+            echo "$default_subname" "$smallDescr"
+            sed -i "${edit_loop}s#$default_subname#$smallDescr#" "$log_Path"
+        elif [ "$check_site" = "https://nanyangpt.com" ]; then
+            source "$AUTO_ROOT_PATH/post/nanyangpt.sh"
+            edit_post_nanyangpt
             echo "$default_subname" "$smallDescr"
             sed -i "${edit_loop}s#$default_subname#$smallDescr#" "$log_Path"
         fi 
     fi
     
     #---clean---#    
-    rm -f $html_page $descr_page $descr_bbcode 
-    descr_bbcode=''
-    descr_page=''
-    html_page=''
+    rm -f "$hds_rss_desc" "$hds_rss_html" "$source_detail_page" "$source_detail_desc"
     new_torrent_name=''
+    source_site_URL=''
+    source_detail_desc=''
+    source_detail_page=''
 done
 
 #------------------------------------#
