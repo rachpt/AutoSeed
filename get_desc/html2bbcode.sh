3796a22e856fdd0fef3c74f3fc80e3b046d06460
rachpt <rachpt@126.com>
2018-06-20 20:27:17 +0800

    副标题剔除空格

--- get_desc/html2bbcode.sh
+++ get_desc/html2bbcode.sh
@@ -2,8 +2,8 @@
 # FileName: get_desc/html2bbcode.sh
 #
 # Author: rachpt@126.com
-# Version: 2.1v
-# Date: 2018-06-15
+# Version: 2.0v
+# Date: 2018-06-10
 #
 #-------------------------------------#
 sed -i "s/id=\"[^\"]\"//g; s/alt=\"[^\"]\"//g" "$source_detail_desc"
@@ -31,7 +31,7 @@ sed -i "s/<font face=\"[cC]ourier [nN]ew\">/[font=monospace]/g;s/<font face=\"mo
 sed -i "s#<table[^>]\+>#[quote]#g;s#</table>#[/quote]\n#g;" "$source_detail_desc"
 
 #---img---#
-sed -i "s#\"[^\"]\+attachments\([^\"]\+\)#\"${source_site_URL}/attachments\1#g" "$source_detail_desc"
+sed -i "s#\"[^\"]*attachments\([^\"]\+\)#\"${source_site_URL}/attachments\1#g" "$source_detail_desc"
 sed -i "s#<img[^>]\+src=\"\(.[^\"]\+\)\"[^>]*>#[img]\1[/img]#g" "$source_detail_desc"
 sed -i "s#\]attachments#\]${source_site_URL}/attachments#g" "$source_detail_desc"
 
@@ -60,7 +60,9 @@ if [ "$source_site_URL" = "https://hdchina.org" ]; then
         tmp_poster_file="$AUTO_ROOT_PATH/tmp/${hdc_poster_url##*/}"
         http --ignore-stdin -dco "$tmp_poster_file" "$hdc_poster_url" "$cookie_source_site"
         new_poster_url="$(http --ignore-stdin -f POST 'https://sm.ms/api/upload' smfile@"$tmp_poster_file"|egrep -o "\"url\":\"[^\"]+\""|awk -F "\"" '{print $4}'|sed 's/\\//g')"
+        new_poster_url_byrbt="$(http --ignore-stdin -f POST 'https://bt.byr.cn/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Images' upload@"$tmp_poster_file" "$cookie_byrbt"|egrep -o 'http[a-zA-Z0-9./:()]+images[a-zA-Z0-9./:()]+'|sed 's/http:/https:/g')"  # byrbt
         sed -i "s#$hdc_poster_url#$new_poster_url#g" "$source_detail_desc"
+        sed -i "s#$hdc_poster_url#$new_poster_url_byrbt#g" "$source_detail_html" # byrbt
         rm -f "$tmp_poster_file"
         hdc_poster_url=''
         tmp_poster_file=''
@@ -69,7 +71,8 @@ if [ "$source_site_URL" = "https://hdchina.org" ]; then
 fi
 
 #---ttg imdb url---#
-sed -i "s#\[url=http://www.imdb.com/title/tt[0-9]\{7\}/\]\[url=http://www.imdb.com/title/tt[0-9]\{7\}/\]\(http://www.imdb.com/title/tt[0-9]\{7\}/\)\[/url\]\[/url\]#\1#g" "$source_detail_desc"
+sed -i "s#\[url=http[s]*://www.imdb.com/title/tt[0-9]\{7\}[/]*\]\[url=http[s]*://www.imdb.com/title/tt[0-9]\{7\}[/]*\]\(http[s]*://www.imdb.com/title/tt[0-9]\{7\}[/]*\)\[/url\]\[/url\]#\1#g" "$source_detail_desc"
+sed -i "s#\[url=http[s]*://movie.douban.com/subject/[0-9]\{8\}[/]*\]\[url=http[s]*://movie.douban.com/subject/[0-9]\{8\}[/]*\]\(http[s]*://movie.douban.com/subject/[0-9]\{8\}[/]*\)\[/url\]\[/url\]#\1#g" "$source_detail_desc"
 
 #---get subname---#
 if [ -n "`grep -i "CH[ST]" "$source_detail_desc"`" ]; then
@@ -77,9 +80,9 @@ if [ -n "`grep -i "CH[ST]" "$source_detail_desc"`" ]; then
 else
     subname_chs_include=''
 fi
-    
-subname_1=`grep "译　　名" "$source_detail_desc" |sed 's/.*译[　 ]*名[　 ]*//;s/<br \/>//g;s/\n//g;s/\r//g'`
-subname_2=`grep "片　　名" "$source_detail_desc" |sed 's/.*片[　 ]*名[　 ]*//;s/<br \/>//g;s/\n//g;s/\r//g'`
+
+subname_1=`grep "译[　 ]*名" "$source_detail_desc" |sed "s/.*译[　 ]*名[　 ]*//;s/<br \/>//g;s/\n//g;s/\r//g;s/[ ]*//g"`
+subname_2=`grep "片[　 ]*名" "$source_detail_desc" |sed "s/.*片[　 ]*名[　 ]*//;s/<br \/>//g;s/\n//g;s/\r//g;s/[ ]*//g"`
 
 if [ -z "$imdbUrl" ]; then
 	imdbUrl="$(grep -o 'tt[0-9]\{7\}' "$source_detail_desc"|head -n 1)"
