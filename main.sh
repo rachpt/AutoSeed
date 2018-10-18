9b6fdd22533bedab9e15a72ae0078a52f9c251df
rachpt <rachpt@126.com>
2018-10-18 21:31:07 +0800

    fix bug

--- main.sh
+++ main.sh
@@ -42,7 +42,7 @@ get_torrent_func()
 if [ -z "$TR_TORRENT_NAME" ]; then
     for oneTorrentID in `"$trans_remote" ${HOST}:${PORT} --auth ${USER}:${PASSWORD} -l|grep '100%'|awk '{print $1}'|sed 's/\*//g'|sort -nr`
     do
-        oneTorrent=`"$trans_remote" ${HOST}:${PORT} --auth ${USER}:${PASSWORD} -t $oneTorrentID -i |grep 'Name'|head -n 1|sed 's/  Name: //g'`
+        oneTorrent=`"$trans_remote" ${HOST}:${PORT} --auth ${USER}:${PASSWORD} -t $oneTorrentID -i |grep 'Name'|head -n 1|sed 's/  Name: //ig'`
         if [ "$new_torrent_name" = "$oneTorrent" ]; then
 	        TR_TORRENT_NAME="$oneTorrent"
 	        TR_TORRENT_DIR=`"$trans_remote" ${HOST}:${PORT} --auth ${USER}:${PASSWORD} -t $oneTorrentID -i |grep 'Location'|head -n 1|awk '{print $2}'`
@@ -61,10 +61,11 @@ function main_loop()
     #---loop for torrent in flexget path ---#
     for i in $(find "$flexget_path" -iname "*.torrent*" |awk -F "/" '{print $NF}')
     do
-   	    new_torrent_name=`$trans_show "${flexget_path}/$i"|grep 'Name'|head -n 1|sed 's/Name: //'`
+   	    new_torrent_name=`$trans_show "${flexget_path}/$i"|grep 'Name'|head -n 1|sed 's/Name: //i;s/\.mkv$//i;s/\.ts$//i;s/\.mp4$//i;s/\.avi$//i'`
         if [ "$new_torrent_name" != "$(echo "$new_torrent_name"|grep -oP '[-\.a-z0-9A-Z@_ ]+')" ]; then
             #---special for non-standard 0day-name---#
-            new_torrent_name="$($trans_show "${flexget_path}/$i"|grep -A 10 'FILES'|egrep -i '[\.0-9]+[ ]*(GB|MB)'|egrep -io '[-\.a-z0-9@ ]+'|tail -n 2|head -n 1|sed 's/^[\. ]\+//;s/\.[a-z4 ]\{3,5\}$//'|sed 's/\.[Ss]ample//')"
+            new_torrent_name="$($trans_show "${flexget_path}/$i"|grep -A 10
+            'FILES'|egrep -i '[\.0-9]+[ ]*(GB|MB)'|egrep -io '[-\.a-z0-9@ ]+'|tail -n 2|head -n 1|sed 's/^[\. ]\+//;s/\.[a-z4 ]\{3,5\}$//i'|sed 's/\.sample//i')"
         fi
 
         if [ "$i" != "${new_torrent_name}.torrent" ]; then
@@ -73,7 +74,7 @@ function main_loop()
         #---.tr file path---#
         torrentPath="${flexget_path}/${new_torrent_name}.torrent"
         #---use dot name save desc---#
-        dot_name="$(echo "$new_torrent_name"|sed "s/[ ]\+/./g;s/\(.*\)\.mp4/\1/g;s/\(.*\)\.mkv/\1/g")"
+        dot_name="$(echo "$new_torrent_name"|sed "s/[ ]\+/./g;s/\(.*\)\.mp4/\1/gi;s/\(.*\)\.mkv/\1/gi")"
 
         #---generate desc before done---#
         if [ ! -s "${AUTO_ROOT_PATH}/tmp/${dot_name}_desc.txt" ]; then
