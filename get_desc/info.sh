41cfac9a671cf351e564ac069b81f41030dca686
rachpt <rachpt@126.com>
2019-01-03 12:37:00 +0800

    fix bug

--- get_desc/info.sh
+++ get_desc/info.sh
@@ -3,7 +3,7 @@
 #
 # Author: rachpt@126.com
 # Version: 3.0v
-# Date: 2018-12-11
+# Date: 2019-01-02
 #
 #-------------------------------------#
 # 复制 nfo 文件内容至简介，如果没有 nfo 文件，
@@ -16,35 +16,36 @@ generate_info_local() {
   # 本地简介大小为零
   if [ ! -s "$source_desc" ]; then
     # 种子文件绝对路径
-    debug_func 'info_0:media'  #----debug---
+    debug_func 'info:mediainfo-start'  #----debug---
     local main_file_dir="${one_TR_Dir}/${one_TR_Name}"
     # 使用 mediainfo 生成种子中体积最大文件的 iNFO
     local info_generated="$($mediainfo "$(find "$main_file_dir" \
-      -type f -exec stat -c "%s %n" {} \;|sort -nr|head -1)"| \
+      -type f -exec stat -c "%s %n" {} \;|sort -nr|head -1|sed -E 's/^[0-9 ]+//')"| \
       sed '/Unique/d;/Encoding settings/d;/Complete name/d;/Writing library/d;/Writing application/d')"
     # 存档
     if [ "$info_generated" ]; then
       echo "$info_generated" > "$source_desc"
-      debug_func 'info_1:media'  #----debug---
+      debug_func "info:mediainfo-gened"  #----debug---
     fi
   fi
 }
 
 # 首先判断是否有 nfo 文件，以及nfo是否下载完成
 read_info_file() {
+  debug_func "info:one_TR_Dir.1[$one_TR_Dir]"  #----debug---
   if [ ! "$one_TR_Dir" ]; then
       one_TR_Dir="$(find "$default_FILE_PATH" -name \
           "$one_TR_Name" 2> /dev/null|head -1)"
       one_TR_Dir="${one_TR_Dir%/*}"
   fi
-  debug_func 'info_2'  #----debug---
+  debug_func "info:one_TR_Dir.2[$one_TR_Dir]"  #----debug---
 
   if [ "$one_TR_Dir" ]; then
     local nfo_file_size=$("$tr_show" "$torrent_Path"| \
       grep -Eo '\.nfo \([0-9\. ]+[kKbB]+\)'|grep -Eo '[0-9]+\.?[0-9]*')
-      if [[ $nfo_file_size ]]; then
-        local nfo_file_path="$(find "${one_TR_Dir}/${one_TR_Name}" -iname '*.nfo'|head -1)"
-        local nfo_file_downloaded=$(stat --format=%s "$nfo_file_path")
+    if [[ $nfo_file_size ]]; then
+      local nfo_file_path="$(find "${one_TR_Dir}/${one_TR_Name}" -iname '*.nfo'|head -1)"
+      local nfo_file_downloaded=$(stat --format=%s "$nfo_file_path")
       if [[ $nfo_file_downloaded ]]; then
         local judge_download_nfo=$((nfo_file_downloaded/100))
         local judge_nfo_file=$(echo "$nfo_file_size * 10"|bc|awk -F '.' '{print $1}')
@@ -53,19 +54,19 @@ read_info_file() {
           [[ ! $charset ]] && charset='iso-8859-1'
           iconv -f "$charset" -t UTF-8 -c "$nfo_file_path"| \
             sed -E "/^ú+$/d" > "$source_desc"
-          debug_func 'info_3:nfo'  #----debug---
+          debug_func 'info:get-nfo-file'  #----debug---
         fi
-      unset charset 
+        unset charset 
       fi
     else
-        generate_info_local
-        debug_func 'info_4:media'  #----debug---
+      generate_info_local
+      debug_func 'info:use-mediainfo'  #----debug---
     fi
     # byrbt bbcode to html
     [ "$enable_byrbt" = 'yes' ] && [ -s "$source_desc" ] && \
-        sed 's/ /\&nbsp; /g;s!$!&<br />!g' "$source_desc" > "$source_html" 
-    debug_func 'info_5:exit'  #----debug---
+      sed 's/ /\&nbsp; /g;s!$!&<br />!g' "$source_desc" > "$source_html" 
   fi
+  debug_func 'info:exit'  #----debug---
 }
 
 #-------------------------------------#
