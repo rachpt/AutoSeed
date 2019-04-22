5b243aafd422acbec128ebdd9ce26106a88a6ae5
rachpt <rachpt@126.com>
2019-04-22 22:51:20 +0800

    fix bug about unrar

--- get_desc/extract.sh
+++ get_desc/extract.sh
@@ -3,7 +3,7 @@
 #
 # Author: rachpt@126.com
 # Version: 3.1v
-# Date: 2019-04-16
+# Date: 2019-04-22
 #
 #-------------------------------------#
 # 提取rar分卷压缩种子，并制作种子文件。
@@ -12,12 +12,11 @@
 
 extract_rar_files() {
 if [[ "$(find "${Tr_Path}/${Torrent_Name}" -type f -iname "*.rar")" ]]; then
-  hold_on # main.sh, sleep some time
-  unrar e "${Tr_Path}/${Torrent_Name}/*.rar" "${Tr_Path}/${Torrent_Name}" 
-  if [[ $? -eq 0 ]]; then
-    # make dot torrent file
+  unrar e -o- -inul "${Tr_Path}/${Torrent_Name}/*.rar" "${Tr_Path}/${Torrent_Name}" 
+  if [[ $? -eq 0 || $? -eq 10 ]]; then
+    # make dot torrent file, 10 means the files to extract already exist 
     \rm -f "${flexget_path}/${Torrent_Name}.torrent"
-    dottorrent -t "https://iptorrents.com" -s 4M -p -x '*.rar' -x '*.r[0-9]*' -x '*.sfv' \
+    $dottorrent -t "https://iptorrents.com" -s 4M -p -x '*.rar' -x '*.r[0-9]*' -x '*.sfv' \
       -c "Powered by rachpt/AutoSeed. https://github.com/rachpt/AutoSeed" \
       "${Tr_Path}/${Torrent_Name}" "${flexget_path}/${Torrent_Name}.torrent"
     [[ $? -ne 0 ]] && debug_func 'extract: failed to make .torrent file!'
