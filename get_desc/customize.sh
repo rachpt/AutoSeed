36b2ec2bbe12769a296853e78e545d31eb186640
rachpt <rachpt@126.com>
2019-07-14 17:56:41 +0800

    优化，减少不必要的重复

--- get_desc/customize.sh
+++ get_desc/customize.sh
@@ -3,7 +3,7 @@
 #
 # Author: rachpt@126.com
 # Version: 3.1v
-# Date: 2019-04-16
+# Date: 2019-07-14
 #
 #-------------------------------------#
 # 本文件对特定资源发布站点进行特殊设置，
@@ -61,8 +61,11 @@ my_dupe_rules() {
   else
     debug_func ':dupe:no-dupe-rules-file!'  #----debug---
   fi
-  # test tracker status
-  is_tracker_down    # static.sh
+  # test tracker's status
+  [[ ${completion:-100} -ge 92 && $HAVE_TESTED != yes ]] && {
+    is_tracker_down      # static.sh
+    HAVE_TESTED='yes'    # 减少重复测试
+  }
 }
 #----------------------------------------#
 
